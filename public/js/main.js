fecda3910d44b13c60378d7fdf1f5e42a1282b52
Yonatan Kogan <yonatan@romotive.com>
2013-11-20 00:03:46 -0800

    Change the setup of the page to more easily organize the default state

--- public/js/main.js
+++ public/js/main.js
@@ -13,34 +13,34 @@ $(function() {
   $('.screen').bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
     var $a = $(this).find('a');
     // Only enable the link once the transition to the front has finished
-    if(!$(this).hasClass('background') && $a.attr('disabled')) {
+    if($(this).hasClass('foreground') && $a.attr('disabled')) {
       $a.removeAttr('disabled');
     }
   });
 
-  $("body").keydown(function(e) {
-    var $screens = $(".screen");
-    var current = undefined;
-    for(var i=0; i < $screens.length; i++) {
-      if (!$screens.eq(i).hasClass('background')) {
-        current = i;
-        break;        
-      }
-    }
-    var next = current;
-    if(e.keyCode == 37) { // left
-      next = ((current - 1) + $screens.length) % $screens.length;
-    }
-    else if(e.keyCode == 39) { // right
-      next = (current + 1) % $screens.length;
-    }
-    selectScreenWithName($screens.eq(next).attr('name'));
-  });
+  // $("body").keydown(function(e) {
+  //   var $screens = $(".screen");
+  //   var current = undefined;
+  //   for(var i=0; i < $screens.length; i++) {
+  //     if (!$screens.eq(i).hasClass('background')) {
+  //       current = i;
+  //       break;        
+  //     }
+  //   }
+  //   var next = current;
+  //   if(e.keyCode == 37) { // left
+  //     next = ((current - 1) + $screens.length) % $screens.length;
+  //   }
+  //   else if(e.keyCode == 39) { // right
+  //     next = (current + 1) % $screens.length;
+  //   }
+  //   selectScreenWithName($screens.eq(next).attr('name'));
+  // });
 
   // Initialize
   var timeoutID = window.setTimeout(function() {
     selectScreenWithName('projects');
-    }, 1000);
+    }, 300);
 
 });
 
@@ -48,10 +48,10 @@ function selectScreenWithName(name) {
     var $selected = $('[name=' + name + ']');
     $(".screen").each(function(index) {
       if ($(this).attr('name') === $selected.attr('name')) {
-        $(this).removeClass('background');
+        $(this).addClass('foreground');
       } else {
         $(this).find('a').attr('disabled', 'true');
-        $(this).addClass('background');
+        $(this).removeClass('foreground');
       }
     });  
 }
